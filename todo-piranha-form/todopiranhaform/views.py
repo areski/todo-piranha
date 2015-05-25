e8f7052d9a309d0b09145e7a736e8d953a765536
areski <areski@gmail.com>
2015-05-25 17:56:00 +0200

    remove unusued files / code

--- todo-piranha-form/todopiranhaform/views.py
+++ todo-piranha-form/todopiranhaform/views.py
@@ -1,18 +1,16 @@
-from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.view import forbidden_view_config
-from pyramid.security import authenticated_userid
 from pyramid.session import check_csrf_token
 from pyramid.security import remember
 from pyramid.security import forget
-from .forms import task_form
+# from .forms import task_form
 
 from pyramid.httpexceptions import HTTPFound
 from pyramid.httpexceptions import HTTPForbidden
-from deform import ValidationFailure
-
-import deform
-import colander
+# import deform
+# from deform import ValidationFailure
+# import colander
+# from pyramid.response import Response
 import datetime
 
 from .usersdb import USERS
@@ -61,6 +59,7 @@ def get_complete_task():
 
 
 def complete_task(taskid):
+    # complete task
     ntasklist = []
     for task in _TASKS['default']:
         if task['taskid'] == taskid:
@@ -80,42 +79,6 @@ def count_items_left():
         if task['status'] == 'ACTIVE':
             count = count + 1
     return count
-
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(TaskModel).filter(TaskModel.taskname == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'todo-piranha-form'}
-
-
-@view_config(route_name='testjson', renderer='json')
-def testjson(request):
-    return {'val': 1}
-
-
-@view_config(route_name='viewform', renderer='templates/viewform.jinja2')
-def viewform(request):
-    # import ipdb; ipdb.set_trace()
-    appstruct = {}
-
-    if 'submit' in request.POST:
-        try:
-            controls = request.POST.items()
-            appstruct = task_form.validate(controls)
-            html = task_form.render(appstruct)
-        except deform.ValidationFailure as e:
-            html = e.render()
-    else:
-        # the request requires a simple form rendering
-        html = task_form.render()
-
-    return {
-        'form': html,
-        'appstruct': appstruct,
-        }
 
 
 @view_config(route_name='todofiltered', renderer='templates/todo.jinja2',
@@ -236,6 +199,15 @@ def logout(request):
     # url = request.resource_url(request.context)
     return HTTPFound(location=url,
                      headers=headers)
+
+
+# @view_config(route_name='home', renderer='templates/mytemplate.pt')
+# def my_view(request):
+#     try:
+#         one = DBSession.query(TaskModel).filter(TaskModel.taskname == 'one').first()
+#     except DBAPIError:
+#         return Response(conn_err_msg, content_type='text/plain', status_int=500)
+#     return {'one': one, 'project': 'todo-piranha-form'}
 
 
 # @forbidden_view_config(renderer='templates/login.jinja2')
